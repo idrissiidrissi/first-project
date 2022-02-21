13d6c7f2a35e95fea414c32845161a7d9a767058
mohamed tazi <idrissi2929@gmail.com>
2022-02-21 11:27:28 -0500

    02/21/22

---
+++ home.js
@@ -0,0 +1,27 @@
+
+//let price = '45';
+
+//const price = 50;
+//let price = 12;
+//price = price + 1;
+//price = price *2;
+//price = price / 2;
+//price = price % 5;
+//price += 5;
+//price -= 5;
+//price *= 5;
+//price /= 5;
+//price %= 5;
+//showMessage(++price);
+//showMessage(price++);
+//showMessage(--price);
+//console.log(price);
+
+//let price = 3 + 2 * 2;
+//let price = 3 * 2 + 2;
+let price = (3 + 2) * 2;
+showMessage(price);
+
+//TextRate = 0.07;
+
+//showMessage(price * TextRate);
