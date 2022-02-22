e63b0eea46526759aafcd4de71ce4c311e7d2eba
mohamed tazi <idrissi2929@gmail.com>
2022-02-22 09:50:54 -0500

    02/22/22

--- home.js
+++ home.js
@@ -19,9 +19,106 @@
 
 //let price = 3 + 2 * 2;
 //let price = 3 * 2 + 2;
-let price = (3 + 2) * 2;
-showMessage(price);
+//let price = (3 + 2) * 2;
+//let price = 1.1 + 1.3;
+//showMessage(price);
+
+//let amount = -20;
+//let amount = 20 - -2;
+//let amount = 20 - (-2);
+//showMessage(amount)
+
+//let amount = 0;
+//showMessage(--amount)
+
+//let message = 'hello world';
+//let message = "hello \"world\"";
+
+//let name = 'sulayman';
+//let message = `hello ${name}`;
+//let message = 'Hello';
+//message = message.toLowerCase();
+//message = message.toUpperCase();
+//message = message.substring(2);
+//message = message.length;
+//showMessage(message)
+
+//let message = '123'
+
+/*let message = 123;
+let message = "123";
+showMessage(typeof message)
+showMessage(message + 2)*/
+
+/*let amount = 123;
+amount = amount.toString();
+showMessage(typeof amount);*/
+
+
+/*let amount = Number.parseFloat("a123.12");
+showMessage(amount);*/
+
+/*let saved = false;
+showMessage(typeof saved);*/
+
+/*let saved = true;
+saved = !saved
+showMessage(saved);
+console.log(saved)*/
+
+/*let saved;
+showMessage(saved);*/
+
+/*let person = {
+    firstName: 'solayman',
+    lastName: 'idrissi'
+};
+//showMessage(typeof person);
+
+showMessage(person.firstName);
+
+
+
+
 
 //TextRate = 0.07;
 
-//showMessage(price * TextRate);
+//showMessage(price * TextRate);*/
+
+
+/*if (false) {
+    showMessage('true');
+}
+
+let price = 20;
+if (price !== 15) {
+    showMessage('discounted');
+}*/
+
+/*if ("0") {
+    showMessage('true');
+}*/
+
+
+/*for  (let i=0; i < 5; i++) {
+    console.log(i);
+}*/
+
+
+
+/*function showHello6() {
+    console.log('here im');
+}
+showHello6();
+showHello6();*/
+
+/*let myFunction =function loggingFunction () {
+    console.log('here im');
+}
+myFunction();*/
+
+/*function getSecretCode(value) {
+    let code =value * 42;
+    return code;
+}
+showHello6(getSecretCode(2));*/
--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
 function showMessage(message) {
     document.getElementById('message').textContent = message;
 }
+function showHello6(hello6) {
+    document.getElementById('hello6').textContent = hello6;
+}
