3b56b2fd3768af61c46c9ffad2e8e49ee752e081
mohamed tazi <idrissi2929@gmail.com>
2022-03-17 18:31:47 -0400

    JS added +

--- home.js
+++ home.js
@@ -5,9 +5,46 @@ hamburger.addEventListener('click',()=>{
 });
 
 
+/*order*/
 
+const openOrderButtons = document.querySelectorAll('[data-order-target]')
+const closeOrderButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
 
+openOrderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const order = document.querySelector(button.dataset.orderTarget)
+    openOrder(order)
+  })
+})
 
+overlay.addEventListener('click', () => {
+  const orders = document.querySelectorAll('.order.active')
+  orders.forEach(order => {
+    closeOrder(order)
+  })
+})
+
+closeOrderButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const order = button.closest('.order')
+    closeOrder(order)
+  })
+})
+
+function openOrder(order) {
+  if (order == null) return
+  order.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeOrder(order) {
+  if (order == null) return
+  order.classList.remove('active')
+  overlay.classList.remove('active')
+}
+
+/*add to cart*/
 
 
 
